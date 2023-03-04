4fb33eb119a53f6cfdfa838c2fb6e31c4cda8c2e
McCormick95 <106946540+McCormick95@users.noreply.github.com>
2023-03-04 20:53:05 +0000

    made changes to unit testing, still not working

--- Course.Test/CourseTest.cs
+++ Course.Test/CourseTest.cs
@@ -1,15 +1,23 @@
+
 namespace Course.Test;
 
 public class CourseTest
 {
     [Fact]
-     public void OfferingsByGoalAndSemester_GoalIDIsNull_ThrowsException()
-        {
-            CourseServices service = new CourseServices();
+    public void OfferingsByGoalAndSemester_GoalIDIsNull_ThrowsException()
+    {
+        CourseServices service = new CourseServices();
 
-            List<CourseOffering> newList = service.getOfferingsByGoalIdAndSemester(null, null);
+        List<CourseOffering> newList = service.getOfferingsByGoalIdAndSemester(null, null);
 
-            Assert.Throws<Exception>(()=> service.getOfferingsByGoalIdAndSemester(null, null));
+        Assert.Throws<Exception>(()=> service.getOfferingsByGoalIdAndSemester(null, null));
 
-        }
+    }
+
+    [Fact]
+     public void OfferingsByGoalAndSemester_GoalIDIsNotNull_ThrowsException()
+    {
+       
+        Assert.Equal(true,true);
+    }
 }
