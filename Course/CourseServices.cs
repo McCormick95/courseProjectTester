24e617cd892e13d1b3d8c3c18f0bda50e206b7d4
McCormick95 <106946540+McCormick95@users.noreply.github.com>
2023-03-04 18:11:56 +0000

    Started new project

---
+++ Course/CourseServices.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections.Generic;
+
+namespace cs330_proj1
+{
+    public class CourseServices
+    {
+        private CourseRepository repo = new CourseRepository();
+
+
+        //As a student, I want to search for course offerings that meet core goals 
+        // so that I can register easily for courses that meet my program requirements
+         public List<CourseOffering> getOfferingsByGoalIdAndSemester(String theGoalId, String semester) {
+          //finish this method during the tutorial 
+          //use the repo to get the data from the database (data store)
+            List<CoreGoal> theGoals = repo.Goals;
+            List<CourseOffering> theOfferings = repo.Offerings;
+                        
+            //Complete any other required functionality/business logic to satisfy the requirement
+            CoreGoal theGoal =null;
+            foreach(CoreGoal cg in theGoals) {
+               if(cg.Id.Equals(theGoalId)) {
+                  theGoal=cg; break;
+            
+               }
+            }
+            if(theGoal==null) throw new Exception("Didn't find the goal");
+            //search list of courses, then for each course, search offerings
+            List<CourseOffering> courseOfferingsThatMeetGoal = new List<CourseOffering>();
+                        
+            foreach(CourseOffering c in theOfferings) {
+               if(c.Semester.Equals(semester) 
+                  && theGoal.Courses.Contains(c.TheCourse) ) 
+               {
+                  courseOfferingsThatMeetGoal.Add(c);
+               }
+            
+            }
+            return courseOfferingsThatMeetGoal;
+        }
+
+        
+        //Add more service functions here, as needed, for the project
+
+        // User Story 2: As a student, I want to see all available courses so that I know what my options are 
+         public List<Course> getCourses() 
+         {
+            List<Course> theCourses = repo.Courses;
+            List<Course> allCourses = new List<Course>();
+
+            foreach(Course c in theCourses){
+                   allCourses.Add(c);
+            }
+
+            return allCourses;
+         }
+
+        /* User Story 3: As a student, I want to see all course offerings by semester, so that I can choose from what's
+           available to register for next semester */
+        public List<CourseOffering> getCourseOfferingsBySemester(String semester) 
+         {
+            List<CourseOffering> theOffering = repo.Offerings;
+            List<CourseOffering> courseOfferingsForThatSemester = new List<CourseOffering>();
+
+            if(semester==null) throw new Exception("Didn't select a semester");
+
+            foreach(CourseOffering co in theOffering){
+               if(co.Semester.Equals(semester)){
+                   courseOfferingsForThatSemester.Add(co);
+               }
+            }
+            return courseOfferingsForThatSemester;
+         } 
+
+
+        /* User Story 4: As a student I want to see all course offerings by semester and department so that I can 
+        choose major courses to register for */
+        public List<CourseOffering> getCourseOfferingsBySemesterAndDept(string semester, string department){
+
+         List<CourseOffering> theOffering = repo.Offerings;
+         List<CourseOffering> courseOfferingsBySemesterAndDept = new List<CourseOffering>();
+
+         if(semester==null) throw new Exception("Didn't select a semester");
+         if(department==null) throw new Exception("Didn't select a department");
+         
+         foreach(CourseOffering co in theOffering){
+
+         }
+
+         foreach(CourseOffering co in theOffering){
+            if(co.Semester.Equals(semester) && co.TheCourse.Name.Substring(0,4).Equals(department)){
+               courseOfferingsBySemesterAndDept.Add(co);
+            }       
+         }
+
+         return courseOfferingsBySemesterAndDept;
+        } 
+
+
+
+
+        /* As a student I want to see all courses that meet a core goal, so that I can plan out
+           my courses over the next few semesters and choose core courses that make sense for me */
+
+        /* As a student I want to find a course that meets two different core goals, so that I can
+        "feed two birds with one seed" (save time by taking one class that will fulfill two 
+          requirements */
+
+        /* As a freshman adviser, I want to see all the core goals which do not have any course offerings 
+           for a given semester, so that I can work with departments to get some courses offered
+           that students can take to meet those goals */
+
+        
+     }
+}
