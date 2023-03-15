ac2537f3c4f213b13cd389369b737396a902b328
McCormick95 <106946540+McCormick95@users.noreply.github.com>
2023-03-15 15:10:22 +0000

    Made changes to testing class

--- CourseProject.Test/CourseTest.cs
+++ CourseProject.Test/CourseTest.cs
@@ -14,12 +14,39 @@ public class CourseTest
     }
 
     [Fact]
-     public void OfferingsByGoalAndSemester_GoalIDIsNotNull_ThrowsException(){
-        private List<Course> courses;
-        private List<CoreGoal> coreGoals;
-        private List<CourseOffering> courseOfferings;
-        private CourseRepository _repo = new CourseRepository(courses, coreGoals, courseOfferings);
+    public void OfferingsByGoalAndSemester_GoalIDIsNotNull_ReturnsList(){
+
+        List<Course> courses = new List<Course>();
+        List<CoreGoal> coreGoals = new List<CoreGoal>();
+        List<CourseOffering> courseOfferings = new List<CourseOffering>();
+
         
-    }
+        Course tc = new Course() {
+            Name="TEST 101",
+            Title="test class",
+            Credits=4.0,
+            Description="this is a test class"
+
+        };
+        courses.Add(tc);
+
+        CoreGoal tg = new CoreGoal() {
+            Id="TG1",
+            Name="Test Goal",
+            Description="description for test",
+            Courses = new List<Course>() {tc}
+        };
+        coreGoals.Add(tg);
+
+        CourseOffering tco = new CourseOffering() {
+            TheCourse=tc,
+            Section="T1",
+            Semester="Spring 2021"
+
+        };
+        courseOfferings.Add(tco);
+        
+        CourseRepository _repo = new CourseRepository(courses, coreGoals, courseOfferings);
+    }    
 }
 }
