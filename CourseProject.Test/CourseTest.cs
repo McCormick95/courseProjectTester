719920d09ab831a527fe64cee3b3ecb4dd1cc38f
McCormick95 <106946540+McCormick95@users.noreply.github.com>
2023-03-28 02:40:56 +0000

    First two tests run.

--- CourseProject.Test/CourseTest.cs
+++ CourseProject.Test/CourseTest.cs
@@ -7,13 +7,45 @@ public class CourseTest
 {
     [Fact]
     public void OfferingsByGoalAndSemester_GoalIDIsNull_ThrowsException(){
-        var repo = new Mock<CourseServices.ICourseRepository>();
+       var repo = new Mock<ICourseRepository>();
+        
+        List<Course> courses = new List<Course>();
+        List<CoreGoal> coreGoals = new List<CoreGoal>();
+        List<CourseOffering> courseOfferings = new List<CourseOffering>();
 
-        CourseServices service = new CourseServices(repo);
+        Course tc = new Course() {
+            Name="TEST 101",
+            Title="test class",
+            Credits=4.0,
+            Description="this is a test class"
 
-        List<CourseOffering> newList = service.getOfferingsByGoalIdAndSemester(null, null);
+        };
+        courses.Add(tc);
+        repo.Setup(m=>m.GetCourses()).Returns(courses);
 
-        Assert.Throws<Exception>(()=> service.getOfferingsByGoalIdAndSemester(null, null));
+        CoreGoal tg = new CoreGoal() {
+            Id="TG1",
+            Name="Test Goal",
+            Description="description for test",
+            Courses = new List<Course>() {tc}
+        };
+        coreGoals.Add(tg);
+        repo.Setup(m=>m.GetCoreGoals()).Returns(coreGoals);
+
+        CourseOffering tco = new CourseOffering() {
+            TheCourse=tc,
+            Section="T1",
+            Semester="Spring 2021"
+
+        };
+        courseOfferings.Add(tco);
+        repo.Setup(m=>m.GetCourseOfferings()).Returns(courseOfferings);
+
+        CourseServices courseSer = new CourseServices(repo.Object);
+
+        //List<CourseOffering> newList = service.getOfferingsByGoalIdAndSemester(null, null);
+
+        Assert.Throws<Exception>(()=> courseSer.getOfferingsByGoalIdAndSemester(null, null));
 
     }
 
@@ -56,10 +88,9 @@ public class CourseTest
 
         CourseServices courseSer = new CourseServices(repo.Object);
 
-        courseSer.getOfferingsByGoalIdAndSemester("TG1", "Spring 2021");
+        List<CourseOffering> m = courseSer.getOfferingsByGoalIdAndSemester("TG1", "Spring 2021");
 
-        
-        Assert.Equal();
+        Assert.Equal(courseOfferings, m);
         //CourseRepository _repo = new CourseRepository(courses, coreGoals, courseOfferings);  
     }    
 }
